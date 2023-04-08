9b2396424f2027589cb714043611de5fa41c5e26
Bryce Cole <brycewcole@gmail.com>
2023-04-08 14:22:50 -0400

    removing message target

--- message_target.py
+++
@@ -1,9 +0,0 @@
-class MessageTarget:
-    def __init__(self, name, number, system_message):
-        self.name = name
-        self.number = number
-        self.system_message = system_message
-
-    @classmethod
-    def from_dict(cls, dict):
-        return cls(dict["name"], dict["number"], dict["system_message"])
